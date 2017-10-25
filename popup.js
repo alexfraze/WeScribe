5792cdd30a5a959211d4b051dd2bce30dad5dba8
alex frazier <afrazie1@andrew.cmu.edu>
2017-10-24 20:08:18 -0400

    working on getting extension to recognize the change to a netflix movie

--- popup.js
+++ popup.js
@@ -105,23 +105,10 @@ function saveBackgroundColor(url, color) {
 // user devices.
 document.addEventListener('DOMContentLoaded', () => {
   getCurrentTabUrl((url) => {
-    console.log(url)
+    if(url.startsWith("https://www.netflix.com/watch")){
+      alert("you are watching netflix")  
+    }
+    alert("not netflix")
     var dropdown = document.getElementById('dropdown');
-
-    // Load the saved background color for this page and modify the dropdown
-    // value, if needed.
-    getSavedBackgroundColor(url, (savedColor) => {
-      if (savedColor) {
-        changeBackgroundColor(savedColor);
-        dropdown.value = savedColor;
-      }
-    });
-
-    // Ensure the background color is changed and saved when the dropdown
-    // selection changes.
-    dropdown.addEventListener('change', () => {
-      changeBackgroundColor(dropdown.value);
-      saveBackgroundColor(url, dropdown.value);
-    });
   });
 });
