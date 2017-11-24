5f27af0f3ce2f49f86299f5406f53fa0c830c576
Erin Dieringer <ediering@andrew.cmu.edu>
2017-11-24 13:37:16 -0600

    login changes started

--- js/login.js
+++ js/login.js
@@ -12,21 +12,26 @@ $(function() {
 
 	//on submit check if valid or not
 	$("#submit").click(function(e) {
-	    e.preventDefault();
-	    var username = $("#username").val();
-	    var password = $("#password").val();
-	    $.ajax({
-		        type: "Post",
-		        url: "http://localhost:5000/auth/signin/" + `${username}/${password}`,
-		        success: function(result) {
-		            console.log("sucess", result);
-		            alert("User found in db");
-		        },
-		        error: function(result) {
-		            console.log("error" + result);
-		            alert("Unauthorized user");
-		        }
-		});
+		if ($(".login-div").is(":hidden")){
+		 //    e.preventDefault();
+		 //    var username = $("#username").val();
+		 //    var password = $("#password").val();
+		 //    $.ajax({
+			//         type: "Post",
+			//         url: "http://localhost:5000/auth/signin/" + `${username}/${password}`,
+			//         success: function(result) {
+			//             console.log("sucess", result);
+			//             alert("User found in db");
+			//         },
+			//         error: function(result) {
+			//             console.log("error" + result);
+			//             alert("Unauthorized user");
+			//         }
+			// });
+			console.log("signin");
+		}  else {
+			console.log("login");
+		}
 	
 	});
 
