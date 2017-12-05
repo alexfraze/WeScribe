2f95f7c5e3de6f37564ffb856323dc699cf480ea
alex frazier <afrazie1@andrew.cmu.edu>
2017-12-05 11:55:16 -0500

    changing highchart timeline

--- js/my_highchart.js
+++ js/my_highchart.js
@@ -1,4 +1,4 @@
-$.ajax({
+/*$.ajax({
                 type: "Get",
                 url: "http://localhost:5000/usage/",
                 success: function(result) {
@@ -8,139 +8,59 @@ $.ajax({
                     console.log("error" + result);
                 }
     });
-
+*/
 
 // work with erin to figure out API data format.
-function callback(result) {
+/*function callback(result) {
     var data = result;
-    console.log(data);
+    console.log(data);*/
+
+      
+
     API_data =[
-        {
-                name: 'Alex',
-                data: [
-                        [Date.UTC(2011, 1, 1), 8],
-                        [Date.UTC(2011, 2, 1), 9],
-                        [Date.UTC(2011, 3, 1), 10],
-                        [Date.UTC(2011, 4, 1), 10],
-                        [Date.UTC(2011, 5, 1), 8],
-                        [Date.UTC(2011, 6, 1), 7],
-                        [Date.UTC(2011, 7, 1), 6],
-                        [Date.UTC(2011, 8, 1), 7],
-                        [Date.UTC(2011, 9, 1), 5],
-                        [Date.UTC(2011, 10, 1), 4],
-                        [Date.UTC(2011, 11, 1), 4]
-                    ]},
-        {
+    {
+        name: 'Alex',
+        data: [0, 12, 15, 17 ,0]
+    },
+    {
         name: "Sally",
-        data: [
-                        [Date.UTC(2011, 1, 1), 1],
-                        [Date.UTC(2011, 2, 1), 3],
-                        [Date.UTC(2011, 3, 1), 1],
-                        [Date.UTC(2011, 4, 1), 1],
-                        [Date.UTC(2011, 5, 1), 1],
-                        [Date.UTC(2011, 6, 1), 4],
-                        [Date.UTC(2011, 7, 1), 3],
-                        [Date.UTC(2011, 8, 1), 6],
-                        [Date.UTC(2011, 9, 1), 7],
-                        [Date.UTC(2011, 10, 1), 7],
-                        [Date.UTC(2011, 11, 1), 8]
-                    ]},
-        {
+        data: [7.0, 6.9, 15.5, 14.5, 6.4]
+    },
+    {
         name: "Erin",
-        data: [
-                        [Date.UTC(2011, 1, 1), 3],
-                        [Date.UTC(2011, 2, 1), 2],
-                        [Date.UTC(2011, 3, 1), 3],
-                        [Date.UTC(2011, 4, 1), 4],
-                        [Date.UTC(2011, 5, 1), 5],
-                        [Date.UTC(2011, 6, 1), 7],
-                        [Date.UTC(2011, 7, 1), 8],
-                        [Date.UTC(2011, 8, 1), 6],
-                        [Date.UTC(2011, 9, 1), 6],
-                        [Date.UTC(2011, 10, 1), 6],
-                        [Date.UTC(2011, 11, 1), 7]
-                    ]}
-            ]
+        data: [8.9, 4.2, 4.7, 8.5, 3]
+    }]
 
 
 
-    Highcharts.chart('container', {
-    credits: {
-        enabled: false
+Highcharts.chart('container', {
+    chart: {
+        type: 'line'
     },
-
-    exporting: {
-        enabled: false
+    title: {
+        text: 'Previous Billing Cycle Netflix Usage'
     },
-
     subtitle: {
-        text: 'Year 2017'
+        text: 'November 2017' 
     },
-
-
+    xAxis: {
+        categories: ['Nov 1-7', 'Nov 8-14', 'Nov 15-21', 'Nov 22-28', 'Nov 29- Dec 5']
+    },
+    yAxis: {
         title: {
-            text: 'My Group Netflix Usage'
-        },
-
-        subtitle: {
-            text: 'Year 2017'
-        },
-
-        yAxis: {
-            title: {
-                text: 'Hours of Netflix Usage'
-            }
-        },
-        xAxis: {
-                type: 'datetime',
-                tickInterval: 30 * 24 * 3600 * 1000,
-                min: Date.UTC(2011, 1, 1),
-                max: Date.UTC(2011, 12, 1),
-                labels: {
-                    rotation: 45,
-                    step: 1,
-                    style: {
-                        fontSize: '13px',
-                        fontFamily: 'Arial,sans-serif'
-                    }
-                },
-                dateTimeLabelFormats: { // don't display the dummy year
-                    month: '%b \'%y',
-                    year: '%Y'
-                }
-            },
-        legend: {
-            layout: 'vertical',
-            align: 'right',
-            verticalAlign: 'middle'
-        },
-
-        plotOptions: {
-            series: {
-                label: {
-                    connectorAllowed: false
-                },
-                pointStart: 2010
-            }
-        },
-
-        series: API_data,
-
-
-        responsive: {
-            rules: [{
-                condition: {
-                    maxWidth: 500
-                },
-                chartOptions: {
-                    legend: {
-                        layout: 'horizontal',
-                        align: 'center',
-                        verticalAlign: 'bottom'
-                    }
-                }
-            }]
+            text: 'Hours Watched'
         }
+    },
+    plotOptions: {
+        line: {
+            dataLabels: {
+                enabled: true
+            },
+            enableMouseTracking: false
+        }
+    },
+    series: API_data
+});
 
-    });
-}
+
+
