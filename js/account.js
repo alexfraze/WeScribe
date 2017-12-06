0e43afadb48ab0488ad4193a2114e7f84d3edb00
Erin Dieringer <ediering@andrew.cmu.edu>
2017-12-05 19:59:41 -0500

    can create groups in api

--- js/account.js
+++ js/account.js
@@ -5,19 +5,57 @@ $(function() {
 
 	// First create credentials 
 	// Credntials returns crential ID
-	// create group with credential ID
-	var users = ["erin", "ediering"]
-	// Test to see if the users part works for groups 
-	$.ajax({
-			        type: "POST",
-			        url: "http://localhost:5000/groups/" + users,
-			        contentType: 'application/json',
-			        success: function(result) {
-			            console.log(result);
-			        },
-			        error: function(result) {
-			            console.log(result);
-			        }
+
+	//Onclick
+	$("#submit").click(function(e){
+		e.preventDefault();
+		var username = $("#username").val();
+		var password = $("#password").val();
+		$.ajax({
+				        type: "POST",
+				        url: "http://localhost:5000/credentials/" + `${username}/${password}`,
+				        success: function(result) {
+				        	// now create group with the credential ID that has been created
+				            createGroup(result._id);
+				        },
+				        error: function(result) {
+				            console.log(result);
+				        }
 		});	
+	});
+
+	function createGroup(credentialID){
+		var name = $("#group-name").val();
+		var pay = $("#cost").val();
+		var date = $("#payment-date").val();
+		$.ajax({
+				        type: "POST",
+				        url: "http://localhost:5000/groups/" + `${name}/${pay}/${date}/${credentialID}`,
+				        success: function(result) {
+				            console.log(result);
+				            addUsers(localStorage.user, result._id);
+				        },
+				        error: function(result) {
+				            console.log(result);
+				        }
+		});	
+	}
+	// add group to users 
+	function addUsers(user, group){
+		$.ajax({
+				        type: "PUT",
+				        url: "http://localhost:5000/users/" + `${user}/${group}`,
+				        success: function(result) {
+				            console.log(result);
+				        },
+				        error: function(result) {
+				            console.log(result);
+				        }
+		});	
+	}
+
 
 });
+
+
+// Get users in their group {}
